cf7334667d1e73869dfb0e358264a6e64ee8806f
nazmussaadatcse <nazmus.bd97@gmail.com>
2022-11-10 19:56:42 +0600

    edit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,7 +5,8 @@ const Cart = ({cart,handleRemoveFromCart}) => {
     //conditional rendering
     //1. element variable
     //2. ternary operator, condition ? true:false
-    //
+    //3. && operator
+    //4. || operator
     let command;
     if(cart.length===0){
         command = <p>Please add at least 1 item</p>
@@ -27,8 +28,14 @@ const Cart = ({cart,handleRemoveFromCart}) => {
                     < button onClick={()=>handleRemoveFromCart(tShirt)}>X</button>
                     </p> )
             }
+            {cart.length===0 || <p className='orange'>Continue buying..</p>}
+            {cart.length==3 && <div className='orange'>
+                <h3>You Added 3 items</h3>
+                <p>Add more..</p>
+                </div>}
             {command}
-            {cart.length!==4 ? <p>Keep adding</p> : <button>Remove All</button>}
+            {cart.length<3 ? <p>Keep adding</p> : <button>Remove All</button>}
+            {cart.length===4 && <button className='orange'>Review Order</button>}
         </div>
     );
 };
